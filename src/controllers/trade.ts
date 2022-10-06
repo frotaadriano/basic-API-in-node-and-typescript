7c368478af54377b2afec49b4e53f4f6038bda94
Adriano Frota <adriano.frota.silva@accenture.com>
2022-10-05 23:22:48 -0300

    aula5 C01P05 Iniciando API w OvernightJS+express

---
+++ src/controllers/trade.ts
@@ -0,0 +1,18 @@
+import { Controller, Get } from '@overnightjs/core';
+import { Request, Response } from 'express';
+
+
+@Controller('trade')
+export class TradeController {
+
+    @Get('')
+    public getTrade(_: Request, res: Response): void {
+        res.send(
+            [
+                {"u":810119359,"s":"BTCBRL","b":"101935.00000000","B":"0.02600000","a":"101977.00000000","A":"0.02576000"}
+            ]
+        );
+    }
+
+
+}
---
+++ src/server.ts
@@ -0,0 +1,33 @@
+//iniciando a configuração inical do servidor
+import './util/module-alias';
+import { Controller, Server } from '@overnightjs/core';
+import bodyParser from 'body-parser';
+import { TradeController } from './controllers/trade';
+import { Application } from 'express';
+
+export class SetupSever extends Server {
+
+    constructor(private port = 3000) {
+        super();
+    }
+
+    public init(): void {
+        this.setupExpress();
+        this.setupControllers();
+    }
+
+    private setupExpress(): void {
+        this.app.use(bodyParser.json());
+    }
+
+    //init do controller
+    private setupControllers(): void {
+        const tradeController = new TradeController();
+        this.addControllers([tradeController]);
+    }
+
+    public getApp(): Application {
+        return this.app;
+    }
+
+} 
