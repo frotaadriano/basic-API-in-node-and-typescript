747d7483c9a57b60165089cc66a32aa6d9522b8b
Adriano Frota <adriano.frota.silva@accenture.com>
2022-11-14 10:15:20 -0300

    Aula 6 - CP01P05

--- test/functional/trade.test.ts
+++ test/functional/trade.test.ts
@@ -1,11 +1,16 @@
-
-
 describe(' my first', () => {
-    it('should return ticket value', async () => {
-        const { body, status } = await global.testRequest.get('/trade');
-        expect(status).toBe(200);
-        expect(body).toEqual([
-            { "u": 810119359, "s": "BTCBRL", "b": "101935.00000000", "B": "0.02600000", "a": "101977.00000000", "A": "0.02576000" }
-        ]);
-    });
+  it('should return ticket value', async () => {
+    const { body, status } = await global.testRequest.get('/trade');
+    expect(status).toBe(200);
+    expect(body).toEqual([
+      {
+        u: 810119359,
+        s: 'BTCBRL',
+        b: '101935.00000000',
+        B: '0.02600000',
+        a: '101977.00000000',
+        A: '0.02576000',
+      },
+    ]);
+  });
 });
